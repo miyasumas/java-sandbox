9bd3dad1b3744fd1aae73b2592df589b2e746376
MIYASAKA Yasumasa <miyasumas@users.noreply.github.com>
2015-12-02 11:33:03 +0900

    XPath の evaluete vs compile (数が少ないとあまり変わらない)

---
+++ src/main/java/com/github/miyasum/sandbox/XPathSpeed.java
@@ -0,0 +1,49 @@
+package com.github.miyasum.sandbox;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.io.Resources;
+
+/**
+ * XPath の処理速度
+ * 
+ * @author MIYASAKA Yasumasa
+ */
+public class XPathSpeed {
+
+	public static void main(String[] args) throws Throwable {
+		File file = new File(Resources.getResource("sample.xml").toURI());
+
+		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = builderFactory.newDocumentBuilder();
+		Document doc = builder.parse(file);
+
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+		XPathExpression expr = xpath.compile("/root/items/item");
+
+		int trial = 1000000;
+
+		Stopwatch watch1 = Stopwatch.createStarted();
+		for (int i = 0; i < trial; i++) {
+			expr.evaluate(doc);
+		}
+		System.out.println("Compile: " + watch1.stop().elapsed(TimeUnit.MILLISECONDS) + "[msec]");
+
+		Stopwatch watch2 = Stopwatch.createStarted();
+		for (int i = 0; i < trial; i++) {
+			xpath.evaluate("/root/items/item", doc);
+		}
+		System.out.println("Compile: " + watch2.stop().elapsed(TimeUnit.MILLISECONDS) + "[msec]");
+	}
+}
