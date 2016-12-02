fff038fccfde0d8bae82c0783e9aceb913d1ae5e
MIYASAKA Yasumasa <miyasumas@users.noreply.github.com>
2016-12-02 18:43:17 +0900

    Add DateTimeFormatterBuilder sample

---
+++ src/main/java/com/github/miyasum/sandbox/DateTimeFormatterSample.java
@@ -0,0 +1,32 @@
+package com.github.miyasum.sandbox;
+
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
+import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
+import static java.time.temporal.ChronoField.NANO_OF_SECOND;
+import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+/**
+ * @author MIYASAKA Yasumasa
+ */
+public class DateTimeFormatterSample {
+
+	public static void main(String[] args) {
+		DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+				.append(DateTimeFormatter.ISO_LOCAL_DATE)
+				.appendLiteral('T')
+				.appendValue(HOUR_OF_DAY, 2)
+				.appendLiteral(':')
+				.appendValue(MINUTE_OF_HOUR, 2)
+				.appendLiteral(':')
+				.appendValue(SECOND_OF_MINUTE, 2)
+				.appendLiteral('.')
+				.appendFraction(NANO_OF_SECOND, 6, 6, false)
+				.appendOffsetId()
+				.toFormatter();
+		System.out.println(ZonedDateTime.now().format(formatter));
+	}
+}
